cd34b3da6a73ff7c4bc1aeaa4cde81be0974c898
三輪真人 <miwa-masato@MacBook-Pro.local>
2020-08-21 15:44:13 +0900

    Updates

---
+++ precache-manifest.ac71f16c3abf31cb0a2d4b80a9b5571b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d8d0fab029c831965ef83b52c00ee7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f89cd961e19e93c60caa",
+    "url": "/monsters-rolodex/static/css/main.37a256e1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89cd961e19e93c60caa",
+    "url": "/monsters-rolodex/static/js/main.de36d343.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
